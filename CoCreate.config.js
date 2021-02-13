844d23590750e77f98516ab7730801c01accd232
frankpagan <frank@cocreate.app>
2021-02-13 20:56:44 +0000

    clean tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "60283ca871064611a678a1a7",
             key: "html",
             data:{
                 name: "Google Auth Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "60283ca871064611a678a1a6",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "60283ca871064611a678a1a7",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/google-auth",
