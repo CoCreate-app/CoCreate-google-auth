c2428f0aab919854a58fb20a22fd4f7e87d9b7b6
frank <frank@cocreate.app>
2021-04-21 18:09:48 +0000

    fix: One package for server and client

---
+++ src/client.js
@@ -0,0 +1,24 @@
+import api from '@cocreate/api'
+const CoCreateGoogleAuth = {
+	id: 'googleauth',
+	actions: [
+		'generateAuthURL'
+	],
+	
+
+	
+	render_generateAuthURL: function (data) {
+        if (data.object == "error") {
+        	console.log(data.data);
+        }
+        location.href = data;
+    },
+	
+}
+
+api.init({
+	name: CoCreateGoogleAuth.id, 
+	module:	CoCreateGoogleAuth,
+});
+
+export default CoCreateGoogleAuth;
