3c85e180c7b8c246f838a8a7fd298e095be2b368
Frank <frank@cocreate.app>
2022-02-24 04:52:11 +0000

    fix: get environemt and keys using dot notation

--- src/server.js
+++ src/server.js
@@ -15,9 +15,7 @@ class CoCreateGoogleAuth {
 		this.wsManager = wsManager;
 		this.init();
 		this.GOOGLE_CLIENT_ID=null;
-		this.GOOGLE_CLIENT_SECRET=null;
-		this.enviroment = 'test';
-		
+		this.GOOGLE_CLIENT_SECRET=null;		
 	}
 	
 	init() {
@@ -25,87 +23,102 @@ class CoCreateGoogleAuth {
 			this.wsManager.on(this.moduleName,(socket, data) => this.GoogleAuthOperations(socket, data));
 		}
 	}
+
 	async GoogleAuthOperations(socket, data) {
-	  console.log("Google Auth socekt")
-	    let that = this;
-        let type = data['type'];
-        const params = data['data'];
-        
-        try{
-      	       let enviroment = typeof params['enviroment'] != 'undefined' ? params['enviroment'] : this.enviroment;
-               let org = await api.getOrg(params,this.moduleName);
-               this.GOOGLE_CLIENT_ID = org['apis.'+this.moduleName+'.'+enviroment+'.GOOGLE_CLIENT_ID'];
-               this.GOOGLE_CLIENT_SECRET = org['apis.'+this.moduleName+'.'+enviroment+'.GOOGLE_CLIENT_SECRET'];
-               console.log(" KEY GOOGLE "+this.GOOGLE_CLIENT_SECRET)
-      	 }catch(e){
-      	   	console.log(this.moduleName+" : Error Connect to api",e)
-      	   	return false;
-      	 }
-        
-      
-        switch (type) {
-            case 'generateAuthURL':
-                const oAuth2Client = await this.getAuthenticatedClient(that, socket, type);
-                const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
-                const res = await oAuth2Client.request({url});
-                api.send_response(that.wsManager, socket, {"type":type,"response":res.data}, this.moduleName)
-
-                break;
-        }
-        
-	}
-	
-	
-	getAuthenticatedClient(that, socket, type) {
-  return new Promise((resolve, reject) => {
-    // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
-    // which should be downloaded from the Google Developers Console.
-    const port = 3004; // before 3000
-    const oAuth2Client = new OAuth2Client(
-      this.GOOGLE_CLIENT_ID,
-      this.GOOGLE_CLIENT_SECRET,
-      'http://server.cocreate.app:'+port+'/oauth2callback'
-    );
-
-    // Generate the url that will be used for the consent dialog.
-    const authorizeUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: 'https://www.googleapis.com/auth/userinfo.profile',
-    });
-
-    // Open an http server to accept the oauth callback. In this simple example, the
-    // only request to our webserver is to /oauth2callback?code=<code class="language-js">
+    let that = this;
+    let params = data['data'];
+    let environment;
+    let action = data['action'];
     
-    const server = http
-      .createServer(async (req, res) => {
-        try {
-          if (req.url.indexOf('/oauth2callback') > -1) {
-            const qs = new url.URL(req.url, 'http://server.cocreate.app:'+port).searchParams;
-            const code = qs.get('code');
-            console.log(`Code is ${code}`);
-            res.end('Authentication successful! Please return to the console.');
-            server.destroy();
+    try{
+      let org = await api.getOrg(data, this.moduleName);
+      if (params.environment){
+        environment = params['environment'];
+        delete params['environment'];  
+      } else {
+        environment = org.apis[this.moduleName].environment;
+      }
+      this.GOOGLE_CLIENT_ID = org.apis[this.moduleName][environment].GOOGLE_CLIENT_ID'];
+      this.GOOGLE_CLIENT_SECRET = org.apis[this.moduleName][environment].GOOGLE_CLIENT_SECRET'];
+    }catch(e){
+      console.log(this.moduleName+" : Error Connect to api",e)
+      return false;
+    }
+    
+    try {
+      switch (action) {
+        case 'generateAuthURL':
+          const oAuth2Client = await this.getAuthenticatedClient(that, socket, action);
+          const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
+          const res = await oAuth2Client.request({url});
+          this.wsManager.send(socket, this.moduleName, { action, response })
+          break;
+      }
+    } catch (error) {
+      this.handleError(socket, action, error)
+    }
+  }
+  
 
-            // Now that we have the code, use that to acquire tokens.
-            const r = await oAuth2Client.getToken(code);
-            // Make sure to set the credentials on the OAuth2 client.
-            oAuth2Client.setCredentials(r.tokens);
-            console.info('Tokens acquired.');
-            resolve(oAuth2Client);
-            
-          }
-        } catch (e) {
-          reject(e);
-        }
-      })
-      .listen(port, () => {
-          api.send_response(that.wsManager, socket, {"type":type,"response":authorizeUrl}, this.moduleName)
-        // // open the browser to the authorize url to start the workflow
-        // open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+  handleError(socket, action, error) {
+    const response = {
+      'object': 'error',
+      'data': error || error.response || error.response.data || error.response.body || error.message || error,
+    };
+    this.wsManager.send(socket, this.moduleName, { action, response })
+  }
+        
+	
+	getAuthenticatedClient(that, socket, action) {
+    return new Promise((resolve, reject) => {
+      // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
+      // which should be downloaded from the Google Developers Console.
+      const port = 3004; // before 3000
+      const oAuth2Client = new OAuth2Client(
+        this.GOOGLE_CLIENT_ID,
+        this.GOOGLE_CLIENT_SECRET,
+        'http://server.cocreate.app:'+port+'/oauth2callback'
+      );
+
+      // Generate the url that will be used for the consent dialog.
+      const authorizeUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: 'https://www.googleapis.com/auth/userinfo.profile',
       });
-    destroyer(server);
-  });
-}
+
+      // Open an http server to accept the oauth callback. In this simple example, the
+      // only request to our webserver is to /oauth2callback?code=<code class="language-js">
+      
+      const server = http
+        .createServer(async (req, res) => {
+          try {
+            if (req.url.indexOf('/oauth2callback') > -1) {
+              const qs = new url.URL(req.url, 'http://server.cocreate.app:'+port).searchParams;
+              const code = qs.get('code');
+              console.log(`Code is ${code}`);
+              res.end('Authentication successful! Please return to the console.');
+              server.destroy();
+
+              // Now that we have the code, use that to acquire tokens.
+              const r = await oAuth2Client.getToken(code);
+              // Make sure to set the credentials on the OAuth2 client.
+              oAuth2Client.setCredentials(r.tokens);
+              console.info('Tokens acquired.');
+              resolve(oAuth2Client);
+              
+            }
+          } catch (e) {
+            reject(e);
+          }
+        })
+        .listen(port, () => {
+            this.wsManager.send(socket, this.moduleName, { action, response:authorizeUrl })
+            // // open the browser to the authorize url to start the workflow
+          // open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+        });
+      destroyer(server);
+    });
+  }
 
 	
 }//end Class 
